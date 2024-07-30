8449a634fa012b83def7f083ecd052386626a02c
정 현 진 <jhj1004v@naver.com>
2024-07-30 18:43:56 +0900

    CHORE : supabaseApi 함수 선언

---
+++ src/api/supabaseApi.ts
@@ -0,0 +1,7 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = 'https://cjqemjjcmehuaavijvxm.supabase.co';
+const supabaseKey = import.meta.env.VITE_SUPABASE_API;
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
