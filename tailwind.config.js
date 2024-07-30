c2ba1d7c9347700477e4790f6e2e38d2652dc775
정 현 진 <jhj1004v@naver.com>
2024-07-30 20:33:24 +0900

    CHORE : tailwind css 커스터마이징

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,73 @@
 export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
-    extend: {},
+    screens: {
+      // => @media (max-width: 1119px) { ... }
+      md: { max: '1199px' }, // tablet
+
+      // => @media (max-width: 743px) { ... }
+      sm: { max: '743px' }, // mobile
+    },
+    extend: {
+      fontFamily: {
+        sans: ['Helvetica', 'Arial', 'sans-serif'],
+      },
+    },
+    fontSize: {
+      '16-500': [
+        '1.6rem',
+        {
+          fontWeight: '500',
+        },
+      ],
+      '16-600': [
+        '1.6rem',
+        {
+          fontWeight: '600',
+        },
+      ],
+      '16-700': [
+        '1.6rem',
+        {
+          fontWeight: '600',
+        },
+      ],
+    },
+    colors: {
+      // 예시) <p className="bg-gray-D">
+      black: {
+        0: '#000000',
+        1: '#171717',
+        3: '#333236',
+        4: '#4B4B4B',
+      },
+      gray: {
+        7: '#787486',
+        9: '#9FA6B2',
+        D: '#D9D9D9',
+        E: '#EEEEEE',
+        F: '#FAFAFA',
+      },
+      white: {
+        DEFAULT: '#FFFFFF',
+      },
+      violet: {
+        5: '#5534DA',
+        F: '#F1EFFD',
+      },
+      red: {
+        DEFAULT: '#D6173A',
+      },
+      green: {
+        DEFAULT: '#7AC555',
+      },
+      orange: {
+        DEFAULT: '#FFA500',
+      },
+      blue: {
+        DEFAULT: '#76A5EA',
+      },
+    },
   },
   plugins: [],
 };
