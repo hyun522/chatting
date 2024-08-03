696d7dd46ce87060b340be45bf937ef557874c8e
정 현 진 <jhj1004v@naver.com>
2024-08-03 10:09:03 +0900

    FEAT :  layout 구현

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
+import Sidebar from '@/components/Sidebar';
+import ChattingListTab from '@/components/ChattingListTab';
+import { useState } from 'react';
 
-function layout() {
+function Layout() {
+  const [isChattingListOpen, setIsChattingListOpen] = useState(false);
+  const location = useLocation();
+
+  const handleChatIconClick = () => {
+    setIsChattingListOpen(!isChattingListOpen);
+  };
+
+  const isAuthRoute =
+    location.pathname === '/signin' || location.pathname === '/signup';
+
   return (
     <div>
+      {!isAuthRoute && <Sidebar onChatIconClick={handleChatIconClick} />}
+      {isChattingListOpen && <ChattingListTab />}
       <Outlet />
     </div>
   );
 }
 
-export default layout;
+export default Layout;
