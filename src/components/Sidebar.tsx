375af979128bced23875811eb904156279532c88
정 현 진 <jhj1004v@naver.com>
2024-08-06 14:54:03 +0900

    FEAT : sidebar 구현

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,3 +1,5 @@
+import { useNavigate } from 'react-router-dom';
+import supabase from '@/api/supabaseApi';
 import { IoChatboxSharp } from 'react-icons/io5';
 import { IoHomeSharp } from 'react-icons/io5';
 import { RiLogoutBoxRFill } from 'react-icons/ri';
@@ -7,20 +9,29 @@ interface SidebarProps {
 }
 
 function Sidebar({ onChatIconClick }: SidebarProps) {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await supabase.auth.signOut();
+    navigate('/signin');
+  };
+
   return (
-    <div className='fixed top-0 left-0 h-screen w-[15rem] flex border-r'>
-      <div className='flex-grow flex flex-col items-center space-y-10 py-[2rem]'>
-        <h2 className="text-[1.8rem] text-gray-700 font-['KimjungchulMyungjo-Bold'] pb-[2rem]">
+    <div className='fixed top-0 left-0 h-screen w-[18rem] flex border-r'>
+      <div className='flex-grow flex flex-col items-center space-y-10 py-[5rem]'>
+        <h2 className="text-24 text-gray-700 font-['KimjungchulMyungjo-Bold'] pb-[2rem]">
           T vvith M
         </h2>
-        <span></span>
         <IoHomeSharp className='w-8 h-8 text-gray-700 cursor-pointer hover:text-black hover:scale-110' />
         <IoChatboxSharp
           className='w-8 h-8 text-gray-700 cursor-pointer hover:text-black hover:scale-110'
           onClick={onChatIconClick}
         />
         <div className='flex-grow'></div>
-        <RiLogoutBoxRFill className='w-8 h-8 text-gray-700  cursor-pointer hover:text-black hover:scale-110' />
+        <RiLogoutBoxRFill
+          onClick={handleLogout}
+          className='w-8 h-8 text-gray-700  cursor-pointer hover:text-black hover:scale-110'
+        />
       </div>
     </div>
   );
