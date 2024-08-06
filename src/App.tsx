09a5f28518a36e28b40b6018ee08fcebbc1fb637
정 현 진 <jhj1004v@naver.com>
2024-08-06 14:54:31 +0900

    CHORE : contextApi  provider 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import { RouterProvider } from 'react-router-dom';
 import router from '@/pages/routers';
 import '@/styles/global.css';
+import { AuthProvider } from '@/context/UserProfileContext';
 
 function App() {
   return (
     <>
-      <RouterProvider router={router()} />
+      <AuthProvider>
+        <RouterProvider router={router()} />
+      </AuthProvider>
     </>
   );
 }
