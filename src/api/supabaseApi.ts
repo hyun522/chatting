612344fd112d0e3e25b70b81ac8dcd9dd3e36eab
정 현 진 <jhj1004v@naver.com>
2024-08-11 14:32:41 +0900

    database 삭제에 따른 주소변경

--- src/api/supabaseApi.ts
+++ src/api/supabaseApi.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://cjqemjjcmehuaavijvxm.supabase.co';
+const supabaseUrl = 'https://uqkqvvmwcyxhaoycnfxj.supabase.co';
 const supabaseKey = import.meta.env.VITE_SUPABASE_API;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
